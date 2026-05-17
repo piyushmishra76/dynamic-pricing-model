0af2137064be6136f0f120e16f6127ed1ae9890e
Piyush kumar Mishra <piyushmishra2525@gmail.com>
2026-05-17 14:47:24 +0530

    Initial ML deployment project

---
+++ app/main.py
@@ -0,0 +1,100 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+
+# Create FastAPI app
+app = FastAPI(
+    title="Dynamic Pricing Prediction API",
+    description="""
+    Predict ride cost using
+    machine learning.
+    """,
+    version="1.0.0"
+)
+
+
+# Load model and preprocessor
+model = joblib.load(
+    "models\dynamic_pricing_model.pkl"
+)
+
+preprocessor = joblib.load(
+    "models\preprocessor.pkl"
+)
+
+
+# Input schema
+class RideInput(BaseModel):
+    Number_of_Riders: int
+    Number_of_Drivers: int
+    Location_Category: str
+    Customer_Loyalty_Status: str
+    Number_of_Past_Rides: int
+    Average_Ratings: float
+    Time_of_Booking: str
+    Vehicle_Type: str
+    Expected_Ride_Duration: float
+
+
+# Home route
+@app.get(
+    "/",
+    tags=["Home"]
+)
+def home():
+    return {
+        "message":
+        "Dynamic Pricing API is running"
+    }
+
+
+# Prediction route
+@app.post(
+    "/predict",
+    tags=["Prediction"]
+)
+def predict(data: RideInput):
+
+    input_data = pd.DataFrame([{
+        "Number_of_Riders":
+        data.Number_of_Riders,
+
+        "Number_of_Drivers":
+        data.Number_of_Drivers,
+
+        "Location_Category":
+        data.Location_Category,
+
+        "Customer_Loyalty_Status":
+        data.Customer_Loyalty_Status,
+
+        "Number_of_Past_Rides":
+        data.Number_of_Past_Rides,
+
+        "Average_Ratings":
+        data.Average_Ratings,
+
+        "Time_of_Booking":
+        data.Time_of_Booking,
+
+        "Vehicle_Type":
+        data.Vehicle_Type,
+
+        "Expected_Ride_Duration":
+        data.Expected_Ride_Duration
+    }])
+
+    processed_data = preprocessor.transform(
+        input_data
+    )
+
+    prediction = model.predict(
+        processed_data
+    )
+
+    return {
+        "Predicted_Ride_Cost":
+        round(float(prediction[0]), 2)
+    }
